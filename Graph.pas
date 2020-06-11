f23300d6782456c0a78335707d55f1373e399802
Песоцкий Владислав Дмитриевич <pesotskiy.vladislav@amtel.club>
2020-06-11 12:34:07 +0300

    класс индикатор достижения компетенций

--- Graph.pas
+++ Graph.pas
@@ -6,6 +6,28 @@ interface
 type
   TDFSColor = (dfsWhite, dfsGray, dfsBlack);
 
+  TIndicator = class
+  strict private
+    FDescription: string;
+    FID: Integer;
+    function GetID: Integer;
+  public
+    constructor Create(const ID: Integer; const Description: string);
+    property ID: Integer read GetID;
+  end;
+
 implementation
+
+
+function TIndicator.GetID: Integer;
+begin
+  Result := FID;
+end;
+
+constructor TIndicator.Create(const ID: Integer; const Description: string);
+begin
+  FID := ID;
+  FDescription := Description;
+end;
 
 end.
